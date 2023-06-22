71d3ef79fca367a4a0edbd9387a7e5bac9792880
acbruschini <bruschini@gmail.com>
2023-06-22 17:24:30 -0300

    first commit

---
+++ Clases/clase_01/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Maria Martha Serra Limas
+        
+      </Text>
+      <Text>Maria Mart 2</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
